bacae7e0b4f1977c71ac6e4c77840abd155494b6
Zavier Paulino <90364004+Zp1021@users.noreply.github.com>
2024-03-20 18:53:53 -0400

    Update ImageFilter.java

--- icode/src/main/java/com/fdmgroup/icode/image/mirror/ImageFilter.java
+++ icode/src/main/java/com/fdmgroup/icode/image/mirror/ImageFilter.java
@@ -1,11 +1,15 @@
 package com.fdmgroup.icode.image.mirror;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.stereotype.Service;
 
 import com.fdmgroup.icode.image.upload.model.ImageInfo;
 import com.fdmgroup.icode.image.upload.util.ImageUtils;
+
 
 @Service("mirror")
 public class ImageFilter {
@@ -20,10 +24,37 @@ public class ImageFilter {
 	public void mirrorFilter(ImageInfo imageInfo) {
 		BufferedImage bufferedImage = utils.getBufferedImage(imageInfo);
 		BufferedImage mirroredImage = utils.getBufferedImage(imageInfo);
+	
 		// ******************** CODE BELOW ****************
+		
+		//Reading Image
+		File file = new File("C:/Images");
+		BufferedImage img = ImageIO.read(file);
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++){
+				//Retrieve pixel contents
+				int pixel = img.getRGB(x, y);
+				//Creating color object from pixel value
+				Color color = new Color(pixel, true);
+				//Getting RGB values
+				int red = color.getRed();
+				int green = color.getGreen();
+				int blue = color.getBlue();
+				//Mirror modification of RGB values
+				int pixel = img.getRGB(x, -y);
+				//Create new color object
+				color = new Color(red, green, blue);
 
+				img.setRGB(x, y, color.getRGB());
+			}
+		}
+
+		file = new File("D:\Images");
+		ImageIO.write(img, "jpg", file);
+		System.out.println("Done...");
  
 		// ******************** CODE ABOVE ******************
+	
 		utils.saveBufferedImage(mirroredImage, imageInfo.getSimpleName(), "mirror");
 	}
 
